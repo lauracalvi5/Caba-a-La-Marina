af360b647d24cb6917a5fbd80849f5029096d6c2
lauracalvi5 <calvilaurita24@gmail.com>
2024-08-13 23:10:39 -0300

    cambios

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Middleware para parsear el cuerpo de las solicitudes
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+// Ruta para la raíz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contacto.html'));
+});
 
 // Ruta para manejar el formulario
 app.post('/submit-form', (req, res) => {
-    const { nombre, email, mensaje } = req.body;
+    const { nombre, apellido, email, telefono, mensaje } = req.body;
 
-    if (!nombre || !email || !mensaje) {
+    if (!nombre || !apellido || !email || !telefono || !mensaje) {
         return res.status(400).send('Por favor completa todos los campos.');
     }
 
@@ -25,18 +30,17 @@ app.post('/submit-form', (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'lamarinabraford@gmail.com', // Tu correo electrónico
-            pass: 'dcbk aoar wvlb eibz' // Tu contraseña
+            user: 'lamarinabraford@gmail.com', // Reemplaza con una variable de entorno
+            pass: 'vryq ssew nrfy xpxz'  // Reemplaza con una variable de entorno
         }
     });
-
+    
     const mailOptions = {
         from: email,
-        to: 'lamarinabraford@gmail.com', // Tu correo electrónico donde recibirás las consultas
+        to: 'lamarinabraford@gmail.com',
         subject: 'Nueva consulta del formulario de contacto',
-        text: `Nombre: ${nombre}\nCorreo Electrónico: ${email}\nMensaje: ${mensaje}`
+        text: `Nombre: ${nombre} ${apellido}\nCorreo Electrónico: ${email}\nTeléfono: ${telefono}\nMensaje: ${mensaje}`
     };
-
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.error('Error al enviar el correo:', error);
@@ -45,11 +49,6 @@ app.post('/submit-form', (req, res) => {
         console.log('Correo enviado:', info.response);
         res.send('Formulario recibido. ¡Gracias!');
     });
-});
-
-// Ruta para servir el archivo HTML
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'contacto.html'));
 });
 
 app.listen(port, () => {
